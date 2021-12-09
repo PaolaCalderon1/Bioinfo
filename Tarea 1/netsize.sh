c399a7a62248214978d6e84226f60208eca83f0a
PaolaCalderon1 <paola.calderon@est.ikiam.edu.ec>
2021-12-08 23:15:26 -0500

    Se coloco los codigos para resolucion del ejercicio 1

--- Tarea 1/netsize.sh
+++ Tarea 1/netsize.sh
@@ -1,1 +1,13 @@
+ls
+#Primero se abrio el directorio donde se encuentra los archivos necesarios para la tarea  
+#Se debe llamar la carpeta de Saavedra2013
+$ cd Saavedra2013
+#Luego se uso el comando cat para abrir uno de los archivos, en este caso n1.txt
+$ cat n1.txt
+#Despues se usa el siguiente codigo para contar las filas y columnas 
+$ wc -l n1.txt ;  head -n1 n1.txt | grep -o " " | wc -l
+#El resultado fue :
+#numero de filas: 97
+#numero de filas: 80
+
 
