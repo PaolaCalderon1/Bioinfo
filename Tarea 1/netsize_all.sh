6015a595c4874568171c069174d0eb2545268b6b
PaolaCalderon1 <94419598+PaolaCalderon1@users.noreply.github.com>
2021-12-09 18:40:20 -0500

    Se añadio el codigo para el ejercicio 2

--- Tarea 1/netsize_all.sh
+++ Tarea 1/netsize_all.sh
@@ -1,1 +1,9 @@
-
+##Escriba una secuencia de comandos que imprima los números de filas y columnas de cada red 
+## Primero se abrio el directorio donde se encuentra los archivos necesarios para la tarea  
+## Se debe llamar la carpeta de Saavedra2013
+$ cd Saavedra2013
+## Luego se uso el comando cat para visualizar los archivos
+$ cat Saavedra2013
+## Despues se usa el comando for para contar las filas y columnas de todos los archivos
+$ for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done{
+#los numeros que salen con .txt son las filas
