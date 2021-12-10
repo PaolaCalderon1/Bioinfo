f2dcb0096ef86877c62c5d3541dc6d2564e7ff09
PaolaCalderon1 <94419598+PaolaCalderon1@users.noreply.github.com>
2021-12-10 12:24:51 -0500

    Se elimino un signo que iba en la linea 8

--- Tarea 1/netsize_all.sh
+++ Tarea 1/netsize_all.sh
@@ -5,5 +5,5 @@ $ cd Saavedra2013
 ## Luego se uso el comando cat para visualizar los archivos
 $ cat Saavedra2013
 ## Despues se usa el comando for para contar las filas y columnas de todos los archivos
-$ for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done{
+$ for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done
 #los numeros que salen con .txt son las filas
